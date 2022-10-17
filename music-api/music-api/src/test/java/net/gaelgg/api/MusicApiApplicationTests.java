ddb74be46005b648a78b694eb878c14ea569282c
edwinperez16 <114115684+Gaelpb17@users.noreply.github.com>
2022-10-16 19:58:10 -0500

    videos 1 a 3

---
+++ music-api/music-api/src/test/java/net/gaelgg/api/MusicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.gaelgg.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
